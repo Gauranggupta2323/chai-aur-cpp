23f3a17751fbf8ae867a23931d9a0d4a06894ae2
Gauranggupta2323 <gauranggupta2310@gmail.com>
2025-06-23 06:08:07 +0530

    Update Function_call_by_value.cpp

--- Function_2.cpp
+++ Function_2.cpp
@@ -1,3 +1,6 @@
+
+// CALL by Value
+
 #include <iostream>
 
 using namespace std;
@@ -5,13 +8,13 @@ using namespace std;
 
 void pourChai(int cups){
     cups = cups + 5;
-    cout << "Poured cups: " << cups << endl;
+    cout << "Poured cups: " << cups << endl;        // poured cups toh 7 aaega 
 }
 
 int main(){
     
-    int cups = 2;
-    pourChai(cups);
-    cout << "Total cups are " << cups << endl;
+    int chaiCups = 2;
+    pourChai(chaiCups);
+    cout << "Total cups are " << chaiCups << endl;          // total cups 2 hi aaega 
     return 0;
 }
