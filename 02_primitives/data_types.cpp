e2a90b52d533bd0f16ac45fa38308789fefc4621
Gauranggupta2323 <gauranggupta2310@gmail.com>
2025-06-23 05:40:28 +0530

    Update and rename data.cpp to data_types.cpp

--- 02_primitives/data_types.cpp
+++ 02_primitives/data_types.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main(){
-
+        
     int teaLeaves = 50;
     float waterTemperature = 85.588588;
     double priceOfTea = 299.99;
     char teaGrade = 'A';
-    bool isTeaReady = false;
+    bool isTeaReady = false;     //   yha pe 1,0,true,false ki jagah kuch bhi likhoge toh true aaega   
 
     cout << waterTemperature << endl;
 
